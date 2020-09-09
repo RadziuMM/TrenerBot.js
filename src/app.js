4d72559ccaee9cd6a5971a9e376d0ed79b2607e2
Radoslaw Mazurczak <RMazurczak@protonmail.com>
2020-09-09 17:12:37 +0200

    deploy

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 const TelegramBot = require('node-telegram-bot-api');
 const Discord = require('discord.js');
+const express = require('express');
 const config = require('./config.json');
 
+const app     = express();
 const client = new Discord.Client();
 const bot = new TelegramBot(config.tokenT, {polling: true});
+
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 
 const users = [];
 
